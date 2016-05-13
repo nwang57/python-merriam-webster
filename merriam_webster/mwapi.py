e9252fcfa221c2340694c43b12f6b1cdb5a4af2d
TianxiongWang <nickwang57@gmail.com>
2016-05-13 23:37:16 +0800

    draft for Thesaurus api

--- merriam_webster/mwapi.py
+++ merriam_webster/mwapi.py
@@ -36,6 +36,21 @@ class MWwrapper:
                 res.append(node.tail.strip())
         return " ".join(res).strip()
 
+    def _parse_reponse(self, response):
+        try:
+            root = ET.fromstring(response.content)
+        except ET.ParseError as e:
+            if re.search("Invalid API key", response.content.decode("utf-8")):
+                raise ValueError("Invalid API Key")
+            else:
+                raise e
+        return root
+
+    def _check_word_found(self, root):
+        suggestion_tags = root.findall('suggestion')
+        if suggestion_tags:
+            suggestions = [s.text for s in suggestion_tags]
+            raise ValueError("Word Not Found. Do you mean %s" % suggestions)
 
 class CollegiateApi(MWwrapper):
 
@@ -45,35 +60,57 @@ class CollegiateApi(MWwrapper):
         super().__init__(key)
 
     def lookup(self, word):
-        """return a generator of the related words"""
+        """look up the word"""
         url = self.build_url(word)
         response = requests.get(url)
-        try:
-            root = ET.fromstring(response.content)
-        except ET.ParseError as e:
-            if re.search("Invalid API key", response.content.decode("utf-8")):
-                raise ValueError("Invalid API Key")
-            else:
-                raise e
-        suggestion_tags = root.findall('suggestion')
-        if suggestion_tags:
-            suggestions = [s.text for s in suggestion_tags]
-            raise ValueError("Word Not Found. Do you mean %s" % suggestions)
-        return self.parse_xml(root)
+        root = self._parse_reponse(response)
+        self._check_word_found(root)
+        return self.parse_xml(root, word)
 
-    def parse_xml(self, root):
+    def parse_xml(self, root, word):
+        """Only parse the matched word profile"""
         for entry in root.findall('entry'):
-            params = {}
-            params['word'] = entry.find('ew').text
-            params['functional label'] = getattr(entry.find('fl'), 'text', None)
-            params['definitions'] = self._get_definitions(entry)
-            sound = entry.find('sound')
-            if sound:
-                params['sound_lists'] = [s.text for s in sound]
-            yield params
+            if entry.find('ew').text == word:
+                params = {}
+                params['word'] = entry.find('ew').text
+                params['functional_label'] = getattr(entry.find('fl'), 'text', None)
+                params['definitions'] = self._get_definitions(entry)
+                sound = entry.find('sound')
+                if sound:
+                    params['sound_lists'] = [s.text for s in sound]
+                return params
 
     def _get_definitions(self, entry):
         df_tags = entry.find('def').findall('dt')
         return [self._flatten_tree(d, exclude=['vi','sx']) for d in df_tags]
+
+
+class Thesaurus(object):
+
+    base_url = "http://www.dictionaryapi.com/api/v1/references/thesaurus/xml/"
+
+    def __init__(self, key):
+        super(Thesaurus, self).__init__(key)
+
+    def lookup(self, word):
+        url = self.build_url(word)
+        response = requests.get(url)
+        self._parse_reponse(response)
+        self._check_word_found(root)
+        return self.parse_xml(root, word)
+
+    def parse_xml(self, root, word):
+        """Only return the matched word profile"""
+        for entry in root.findall("entry"):
+            term = entry.find('term')
+            if term.find('hw').text == word:
+                params= {}
+                params['word'] = word
+                params['functional_label'] = getattr(term.find('fl'), 'text', None)
+                params['senses'] = self._get_sense(self, entry):
+
+    def _get_sense(self, entry):
+        pass
+
 
 
--- merriam_webster/test.py
+++ merriam_webster/test.py
@@ -5,10 +5,13 @@ from mwapi import CollegiateApi
 class MWbaseTestCase(unittest.TestCase):
     def setUp(self):
         """Initializes the instance of MWapi"""
-        self.mwapi = CollegiateApi("08a56188-ee0a-46cb-a456-4780ace154df")
+        pass
 
 
 class CollegiateTests(MWbaseTestCase):
+    def setUp(self):
+        self.mwapi = CollegiateApi("08a56188-ee0a-46cb-a456-4780ace154df")
+
     def test_build_url(self):
         word = "python"
         expected_url = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/%s?key=08a56188-ee0a-46cb-a456-4780ace154df" % word
@@ -16,15 +19,11 @@ class CollegiateTests(MWbaseTestCase):
         self.assertEqual(expected_url, url)
 
     def test_lookup(self):
-        word_gen = self.mwapi.lookup("protocol")
-        word_list = list(word_gen)
-        self.assertEqual(2, len(word_list))
+        word = self.mwapi.lookup("protocol")
+        self.assertEqual("protocol", word["word"])
 
     def test_attribute_parsing(self):
-        word_gen = self.mwapi.lookup("protocol")
-        word_list = list(word_gen)
-        self.assertEqual(2, len(word_list))
-        word = word_list[0]
+        word = self.mwapi.lookup("protocol")
         expected_def = ":a preliminary memorandum often formulated and signed by negotiators as a basis for a final convention or treaty"
         self.assertEqual(expected_def, word['definitions'][1])
 
@@ -35,12 +34,25 @@ class CollegiateTests(MWbaseTestCase):
 
     def test_invalid_apikey(self):
         self.mwapi.apikey = "22abc"
-        print(self.mwapi.build_url("wtx"))
         with self.assertRaises(ValueError) as e:
             _ = self.mwapi.lookup("protocol")
         self.assertEqual(str(e.exception), "Invalid API Key")
 
+class ThesaurusTests(MWbaseTestCase):
+    def setUp(self):
+        self.mwapi = ThesaurusApi("3a554019-ce58-4a54-99e5-d1754a6c098c")
 
+    def test_build_url(self):
+        word = "python"
+        expected_url = "http://www.dictionaryapi.com/api/v1/references/thesaurus/xml/%s?key=3a554019-ce58-4a54-99e5-d1754a6c098c" % word
+        self.assertEqual(expected_url, self.mwpi.build_url(word))
+
+    def test_lookup(self):
+        word = self.mwpi.lookup("preclude")
+
+
+    def test_attribute_parsing(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
